a77fc23679f3fba3f996dbb798a6e74ccc1835f3
Samuel Howard <srhoward@ucdavis.edu>
2024-03-02 12:52:00 -0800

    working on colab

--- WebServerPROB-1.py
+++ WebServerPROB-1.py
@@ -1,60 +1,54 @@
 # Import socket module
-from socket import *    
+from socket import *
 
 # Create a TCP server socket
-#(AF_INET is used for IPv4 protocols)
-#(SOCK_STREAM is used for TCP)
+# (AF_INET is used for IPv4 protocols)
+# (SOCK_STREAM is used for TCP)
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
-serverPort = 8080
-serverSocket.bind(('localhost', serverPort)) 
+serverPort = 6789
+serverSocket.bind(("", serverPort))
 serverSocket.listen(1)  # Listen for incoming connections, allowing only one connection at a time
-
-
 
 # Server should be up and running and listening to the incoming connections
 while True:
-	print('Ready to serve...')
-	
-	# Set up a new connection from the client
-	connectionSocket, addr = serverSocket.accept()  # Accept a new connection
-	
-	# If an exception occurs during the execution of try clause
-	# the rest of the clause is skipped
-	# If the exception type matches the word after except
-	# the except clause is executed
-	try:
-		# Receives the request message from the client
-		message =  connectionSocket.recv(1024).decode()
+    print('Ready to serve...')
+    
+    # Set up a new connection from the client
+    connectionSocket, addr = serverSocket.accept()  # Accept a new connection
+    
+    # If an exception occurs during the execution of try clause
+    # the rest of the clause is skipped
+    # If the exception type matches the word after except
+    # the except clause is executed
+    try:
+        # Receives the request message from the client
+        message = connectionSocket.recv(1024).decode()
 
-		# Extract the path of the requested object from the message
-		# The path is the second part of HTTP header, identified by [1]
-		filename = message.split()[1]
+        # Extract the path of the requested object from the message
+        # The path is the second part of HTTP header, identified by [1]
+        filename = message.split()[1]
 
-		# Because the extracted path of the HTTP request includes 
-		# a character '\', we read the path from the second character 
-		f = open(filename[1:])
+        # Because the extracted path of the HTTP request includes 
+        # a character '\', we read the path from the second character 
+        with open(filename[1:], "rb") as f:
+            # Store the entire content of the requested file in a temporary buffer
+            outputdata = f.read()
 
-		# Store the entire contenet of the requested file in a temporary buffer
-		outputdata = f.read()
-
-		# Send the HTTP response header line to the connection socket
-		connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
+        # Send the HTTP response header line to the connection socket
+        connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
  
-		# Send the content of the requested file to the connection socket
-		for i in range(0, len(outputdata)):  
-			connectionSocket.send(outputdata[i].encode())
-	
-		
-		# Close the client connection socket
-		connectionSocket.close()
+        # Send the content of the requested file to the connection socket
+        connectionSocket.sendall(outputdata)
+    
+        # Close the client connection socket
+        connectionSocket.close()
 
-	except IOError:
-		# Send HTTP response message for file not found
-		connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
+    except IOError:
+        # Send HTTP response message for file not found
+        connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
         
-		# Close the client connection socket
-		connectionSocket.close()
+        # Close the client connection socket
+        connectionSocket.close()
 
-serverSocket.close()  
-
+serverSocket.close()
